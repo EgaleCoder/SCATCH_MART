7ab4ea1681b1aea16d6e754ecdc9d507696aa595
iron abhi <146490203+ironabhi05@users.noreply.github.com>
2025-10-14 08:11:18 +0530

    Update AdminLogin.jsx

--- src/Pages/AdminLogin.jsx
+++ src/Pages/AdminLogin.jsx
@@ -44,7 +44,6 @@ const Form = () => {
         <div className="min-h-screen flex items-center justify-center bg-slate-100 px-4">
           <AuthFormSkeletonLogin />
         </div>
-        <FooterSpacer />
       </>
     );
   }
